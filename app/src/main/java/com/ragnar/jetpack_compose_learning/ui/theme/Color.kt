e65556fbd46132fe970c967b18cb7e9db9078079
Shubhash Singh <subhashsingh2059@gmail.com>
2025-09-16 11:14:16 +0530

    added button to display text from user input

---
+++ app/src/main/java/com/ragnar/jetpack_compose_learning/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.ragnar.jetpack_compose_learning.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MyPurple = Color(0xFF6200EE)
+val MyTeal = Color(0xFF03DAC5)
+val MyBlack = Color(0xFF000000)
